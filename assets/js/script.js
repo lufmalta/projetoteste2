16000b7e2b3d22af093a46788ecfe39245b710cb
Luiz Fernando <lufmalta@gmail.com>
2018-09-19 15:44:03 -0300

    JS - Eventos Teclado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-function eventoMostrar(){
-		var tipo = document.getElementById("senha").type;
-		if(tipo == "password"){
-			document.getElementById("senha").type = "text";
-			document.getElementById("mosSenha").innerHTML = "ESCONDER";
-			document.getElementById("mosSenha").style.marginLeft = "140px";
-		}else {
-			document.getElementById("senha").type = "password";
-			document.getElementById("mosSenha").innerHTML = "MOSTRAR";
-			document.getElementById("mosSenha").style.marginLeft = "145px";
-		}
-	 
+function clicouTeclado(event){
+	console.log("Clicou tecla: " +event.keyCode);
+}
+function soltouTecla(event){
+	console.log("Soltou Tecla"+event.keyCode);
+}
+function apertouTecla(event){
+	console.log("Apertou tecla" +event.keyCode);
+	if(event.shiftKey == true && event.keyCode == 69){
+		console.log("Clicou o botao shift + o E");
+	}
 }

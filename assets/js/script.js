41588e1ed8bcc6b15a96d75b792d36c5c0786c71
Luiz Fernando <lufmalta@gmail.com>
2018-09-19 15:09:10 -0300

    JS - Ações de mouse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,15 @@
-function mudarNumero(){
-	var n1 = document.getElementById("n1").innerHTML;
-	var n2 = document.getElementById("n2").value;
-
-	if(n1 == n2){
-		alert("Parabens, voce acertou");
-		document.getElementById("n2").value = "";
-		
-	}else if(n2 == "") {
-		alert("é necessario digitar algum numero");
-		document.getElementById("n2").value = "";
-
-	}else{
-		alert("Infelizmente voce errou");
-		document.getElementById("n2").value = "";
-	}
-	resetar();
+function passouMouse(){// 顱uando passa o mouse no botao
+	console.log("Passou o mouse");
 }
-
-function resetar(){
-	var numalea =  Math.floor(Math.random () * 100);//math random pega um n򭥲o aleatorio qualquer
-		//e o valor esta entre 0 e 10, todavia 页m numero real e n㡩nteiro
-		//o comando Math.floor faz o numero virar inteiro.
-		document.getElementById("n1").innerHTML = numalea;
+function clicouMouse(){// 顱uando clica no botao
+	console.log("Clicou o Mouse");
+}
+function saiuMouse() {// 顱uando tira o mouse do botao
+	console.log("Tirou o mouse");
+}
+function soltouBotao(){//isso 顱uando solta o botao do mouse dps de clicar
+	console.log("Soltou botao");                                                                                                                                                                                                                                                                                       
+}
+function moverMouse(){//isso 顱uando o mouse mover dentro do botao
+	console.log("mouse moveu");
 }

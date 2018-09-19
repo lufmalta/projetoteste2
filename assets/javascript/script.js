5b3656f22252dba6acb54276aebd4e4762cb97cf
Luiz Fernando <lufmalta@gmail.com>
2018-09-19 14:48:58 -0300

    JS - Calculos de matematica

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,32 @@
-function adicionarIngredientes(){
-	var ing = document.getElementById("ingrediente").value;
-	var listahtml = document.getElementById("lista").innerHTML;
-	listahtml = listahtml + "<li>"+ing+"</li>";
-	document.getElementById("lista").innerHTML = listahtml;  
-	document.getElementById("ingrediente").value = "";
-
+function somar(){
+	var camp1 = parseInt(document.getElementById("campo1").value);
+	var camp2 = parseInt(document.getElementById("campo2").value);
+	var resultado = camp1 + camp2;
+	alert("A soma dos dois campos é: "+resultado);
+	document.getElementById("campo1").value = "";
+	document.getElementById("campo2").value = "";
+}
+function subtrair(){
+	var camp1 = parseInt(document.getElementById("campo1").value);
+	var camp2 = parseInt(document.getElementById("campo2").value);
+	var resultado = camp1 - camp2;
+	alert("A subtração dos campos é: "+resultado);
+	document.getElementById("campo1").value = "";
+	document.getElementById("campo2").value = "";
+}
+function multiplicar(){
+	var camp1 = parseInt(document.getElementById("campo1").value);
+	var camp2 = parseInt(document.getElementById("campo2").value);
+	var resultado = camp1 * camp2;
+	alert("A multiplicação é: " +resultado);
+	document.getElementById("campo1").value = "";
+	document.getElementById("campo2").value = ""; 
+}
+function dividir() {
+	var camp1 = parseInt(document.getElementById("campo1").value);
+	var camp2 = parseInt(document.getElementById("campo2").value);
+	var resultado = camp1 / camp2 ;
+	alert("A divisão é: " +resultado);
+	document.getElementById("campo1").value = "";
+	document.getElementById("campo2").value = "";
 }

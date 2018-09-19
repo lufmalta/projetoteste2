8f96febf6346e77bc692826a0f266bf6e44ce260
Luiz Fernando <lufmalta@gmail.com>
2018-09-19 15:51:59 -0300

    JS- Eventos Formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
-function paginaCarregada(){
-	alert("pagina carregou");
+function apagarTexto(){
+	var campTexto = document.getElementById("campoTexto").value;
+	campTexto = "";
+	document.getElementById("campoTexto").value = campTexto;
+	sumirApagar();
 }
-//também pode fazer assim:
-
-// window.onload = function(){
-// 	alert("pagina carregou");
-// }
+function mostrarApagar(){
+	var apagar = document.getElementById("texto").style.display;
+	apagar = "block";
+	document.getElementById("texto").style.display = apagar;
+}
+function sumirApagar(){
+	var apagar = document.getElementById("texto").style.display;
+	apagar = "none";
+	document.getElementById("texto").style.display = apagar;
+}
+function quandoMudar(obj){
+	console.log("Mudou para: "+obj.value);
+}
+function focou(){
+	console.log("Focou!");
+}
+function saiuFoco(){
+	console.log("Saiu do foco");
+}
+function apertouSubmit(){
+	document.write("Enviou formulário");
+}

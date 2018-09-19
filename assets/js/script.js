ff39a4ef0d40a392f970d2c5aaddf93ca8761dbf
Luiz Fernando <lufmalta@gmail.com>
2018-09-19 16:17:19 -0300

    JS - Manipulando Formulario(simples)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,13 @@
-var interval = setInterval(addBola, 1000);
-
-function addBola(){
-	var bolinha = document.createElement("div");
-	bolinha.setAttribute("class", "bola");
-	var p1 = Math.floor(Math.random() * 900);
-	var p2 = Math.floor(Math.random() * 400);
-	var arrayCores = ["A","B","C","D","E","F","0","1","2","3","4","5","6","7","8","9"];
-	var cor = null;
-	var coresSelect = "#";
-	for(i = 0;i <= 5; i++){
-		cor = Math.floor(Math.random() * 16);
-		coresSelect = coresSelect + arrayCores[cor];
+function validar() {
+	var text = document.getElementById("texto").value;
+	if(text.length == ""){
+		alert("esta vazio");
+		return false;
+	}else if(text.length == 2){
+		alert("esta preenchido");
+		return true;
+	}else {
+		alert("precisa ter 2 algarismos, nada menos e nada mais do que isto.");
+		return false;
 	}
-	bolinha.setAttribute("style","background-color:"+coresSelect+";left:"+p1+"px;top:"+p2+"px;");
-	bolinha.setAttribute("onclick","estourar(this)"); 
-	document.body.appendChild(bolinha);
-}
-function estourar(elemento){
-	document.body.removeChild(elemento);
-}
-function parar(){
-	clearInterval(this.interval);
-	document.getElementById('voltar').disabled = false;
-}
-function voltar(){
-	interval = setInterval(addBola, 1000);
-	document.getElementById('voltar').disabled = true;
 }

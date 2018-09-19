6bd3d5e2cc044d3df30cc566f31c3f2472015adc
Luiz Fernando <lufmalta@gmail.com>
2018-09-19 16:18:54 -0300

    JS- Manipulando Canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,28 @@
-function validar() {
-	var text = document.getElementById("texto").value;
-	if(text.length == ""){
-		alert("esta vazio");
-		return false;
-	}else if(text.length == 2){
-		alert("esta preenchido");
-		return true;
-	}else {
-		alert("precisa ter 2 algarismos, nada menos e nada mais do que isto.");
-		return false;
-	}
-}
+var c = document.getElementById("canvas");
+	var ctx = c.getContext("2d");
+	
+	ctx.moveTo(70,230);
+	ctx.lineTo(70,150); 
+
+	ctx.moveTo(70,230);
+	ctx.lineTo(130,230);
+
+	ctx.moveTo(150,230);
+	ctx.lineTo(150,150); 
+
+	ctx.moveTo(150,230);
+	ctx.lineTo(200,230);
+
+	ctx.lineTo(200, 150);
+
+	ctx.moveTo(220, 230);
+	ctx.lineTo(220, 150);
+
+	ctx.moveTo(250,150);
+	ctx.lineTo(300, 150);
+
+	ctx.lineTo(243,230);
+
+	ctx.lineTo(303,230);
+
+	ctx.stroke();

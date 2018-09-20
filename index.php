8f0b82023c52fc09b4604ce8744f0f8e938bafcb
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 13:16:56 -0300

    Bootstrap4- Inicio-topo-section-caroulse

--- index.php
+++ index.php
@@ -7,58 +7,83 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css">
 	<title>Projeto Bootstrap 4</title>
 	<style type="text/css">		
-		.lista ul{
-			list-style:none;
+		#active {
+			background-color:#39aae1;
+			color:#FFF;
 		}
-		.lista li{
+		
+		.li {
+			float:left;
+			margin-left:20px;
+			
+		}
+		.li a {
+			padding:15px;
+			font-size:20px;
+			height:100px;
 			color:#000;
-			float:right;
-			margin-left:7px;
-			margin-right:7px;
+			text-decoration:none;
 		}
-		.lista li:hover,
-		.botao a:hover {
-			background-color:#c2c2c2;
+		.li a:hover {
+			background-color:#39aae1;
+			color:#FFF;
 		}
-		.botao a {
-			text-decoration:none;color:#000;
-			margin-left:7px;
-			margin-right:7px;
+		.carousel-item img{
+			
+			height:650px;
 		}
 
 	</style>
 </head>
-<body>
+<body style="margin-top:30px;">
 	
-	<header  style="background-color:#DDD">
-			<div class="container d-flex justify-content-between" style="background-color:#CCC;">
-				<div class="botao">
-
-					<a href="#" >Home</a>
-					<a href="#" >Contato</a>
-					<a href="#" >Sobre</a>
-
-				</div>
-				<div class="lista">
-
-					<ul>								
-						<a href="#" ><li>Twitter</li></a>
-						<a href="#" ><li>Gmail</li></a>
-						<a href="#" ><li>Facebook</li></a>
-					</ul>
-
-				</div>
-				
-			</div>			
-		</header>
-
-		<section  style="background-color:pink;">			
-			<div class="container" style="background-color:yellow;">
-				
-				
-
-			</div>
-		</section>
+	<header>
+ 		<div class="container d-flex justify-content-around" style="">
+ 			<div class="banner" style="width:300px;">
+ 				<img src="assets/images/cruz.png" width="300" height="110">
+ 			</div>
+ 			<div class="menu" style="height:100px;line-height:50px;">
+ 				<div class="ul">
+ 					<div class="li"><a href="#" id="active">Home</a></div>
+ 					<div class="li"><a href="#">Sobre</a></div>
+ 					<div class="li"><a href="#">Contato</a></div>
+ 					<div class="li"><a href="#">Perfil</a></div>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</header>
+ 	<section>
+ 		<div class="container">
+ 			<div class="carousel slide" id="slideShow">
+ 				<ol class="carousel-indicators">
+ 					<li data-target="#slideShow" data-slide-to="0"></li>
+ 					<li data-target="#slideShow" data-slide-to="1"></li>
+ 					<li data-target="#slideShow" data-slide-to="2"></li>
+ 					<li data-target="#slideShow" data-slide-to="3"></li>
+ 				</ol>
+ 				<div class="carousel-inner">
+ 					<div class="carousel-item active">
+ 						<img src="assets/images/foto1.jpg" class="w-100" >
+ 					</div>
+ 					<div class="carousel-item">
+ 						<img src="assets/images/foto2.jpg" class="w-100">
+ 					</div>
+ 					<div class="carousel-item">
+ 						<img src="assets/images/foto3.jpg" class="w-100">
+ 					</div>
+ 					<div class="carousel-item">
+ 						<img src="assets/images/foto2.jpg" class="w-100">
+ 					</div>
+ 				</div>
+ 				<a class="carousel-control-prev" href="#slideShow" data-slide="prev">
+ 					<span class="carousel-control-prev-icon"></span>
+ 				</a>
+ 				<a class="carousel-control-next" href="#slideShow" data-slide="next">
+ 					<span class="carousel-control-next-icon"></span>
+ 				</a>
+ 			</div>
+ 		</div>
+ 	</section>
 			
 
 			

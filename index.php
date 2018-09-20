4b8bc29cd0817e24ddee90a51a50cd98f3cc61d5
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 16:55:41 -0300

    PHP-OO- Heranca classes

--- index.php
+++ index.php
@@ -1,36 +1,31 @@
 <!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css"> -->
 
 <?php 
-	class Post{
-		private $titulo;
-		private $data;
-		private $corpo;
-		private $comentarios;
-		private $qtComentarios;	
-
-	public function addComentarios($msg){
-		$this->comentarios[] = $msg; 
-		$this->ContarComentarios();
+	class Personagem{
+		private $nome;
+		private $caracteristica;
+	}
+	class Guerreiro extends Personagem{
+		private $atributos;
+		private $classe;
+		private $forca;
+	}
+	class Druida extends Personagem{
+		private $atributos;
+		private $classe;
+		private $stamina;
+	}
+	class Mago  extends Personagem{
+		private $atributos;
+		private $classe;
+		private $inteligencia;
 	}
 
-	public function getQtComentarios(){
-		return $this->qtComentarios;
-	}
-	private function ContarComentarios(){
-		$this->qtComentarios = count($this->comentarios);
-	}
-}
+$warrior = new Guerreiro();
+$warrior->nome = "Argoven";
+echo $warrior->nome;
 
-$post = new Post();
-$post->addComentarios("Teste 1");
-$post->addComentarios("Teste 2");
-$post->addComentarios("Teste 3");
-$post->addComentarios("Teste 4");
-$post->addComentarios("Novo comentario");
-
-echo "Quantidade de comentarios: ".$post->getQtComentarios();
 
 ?>
-
 	<!-- <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap4-js/bootstrap.bundle.min.js"></script> -->

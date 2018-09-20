56a11de0057aab44d4e510b25842cedeb254a334
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 17:23:29 -0300

    PHP-OO- Filtrando-tabela-idade

--- config.php
+++ config.php
@@ -1,34 +1,27 @@
 <?php 
-/*
-* Classe Config
-* Nesta classe realiza a conexao com o banco de dados
-*
-* 
-* @package ProjetoTeste2
-* @author Luiz Fernando <lufmalta@gmail.com>
-*/
-
-class Config{
+class Banco{
 	private $dsn;
 	private $dbuser;
 	private $dbpass;
 	private $pdo;
 
 	public function __construct(){
-		$this->dsn = "mysql:dbname=blog;host=localhost";
-		$this->dbuser = "root";
-		$this->dbpass = "";
-		$this->createPdo();
-	}
-
-	private function createPdo(){
-		$this->pdo = new PDO($this->dsn, $this->dbuser, $this->dbpass);
-		$this->pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+		$this->dsn = "mysql:dbname=projeto_filtrotabela;host=localhost";
+	 	$this->dbuser = "root";
+	 	$this->dbpass = "";
+	 	try{
+	 		$this->pdo = new PDO($this->dsn, $this->dbuser, $this->dbpass);
+	 		$this->pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+	 	}catch(PDOException $e){
+	 		die($e->getMessage());
+	 	}
 	}
 	public function getPdo(){
 		return $this->pdo;
 	}
-
 }
+
+
+	 
 
 ?>
--- index.php
+++ index.php
@@ -1,32 +1,79 @@
 <!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css"> -->
 
-<h1>Digite aqui o seu email ou cpf</h1>
-<form method="POST">
-	<input type="text" name="conta"/>
-	<input type="submit" value="Pesquisar"/>
-</form>
-<hr/>
 <?php 
-if(!empty($_POST['conta'])){
-	$dado = addslashes($_POST['conta']);
-	try{
-		$pdo = new PDO("mysql:dbname=projeto_pesquisacolunas","root","");
-		$pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
-	}catch(PDOException $e){
-		echo "Erro: ".$e->getMessage();
-	}
-
-	$sql = "SELECT * FROM usuarios WHERE (email = :email) OR (cpf = :cpf)";// o OR é no caso de ser alguma das duas opcoes, se quiser que ambas sejam validas, coloca-se o AND
-	$sql = $pdo->prepare($sql);
-	$sql->bindValue(":email", $dado);
-	$sql->bindValue(":cpf", $dado);
-	$sql->execute();
-	if($sql->rowCount() > 0){
-		$sql = $sql->fetch();
-		echo $sql['nome'];
-	}
-
+require "config.php";
+$conecta = new Banco();
+if(empty($_POST['idade'])){
+	$idade = '';
 }
+
+
+if(isset($_POST['idade'])){
+	$idade = $_POST['idade'];
+	if($idade == 1){
+		$sql = "SELECT * FROM usuarios ORDER BY idade DESC";
+		$sql = $conecta->getPdo()->query($sql);
+	}else if($idade == 2){
+		$sql = "SELECT * FROM usuarios ORDER BY idade ASC";
+		$sql = $conecta->getPdo()->query($sql);
+	}else {
+		$sql = "SELECT * FROM usuarios";
+		$sql = $conecta->getPdo()->query($sql);
+	}
+}else {
+	$idade = '';
+	$sql = "SELECT * FROM usuarios";
+	$sql = $conecta->getPdo()->query($sql);
+} 
+			
+
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tabela de Usuarios</title>
+</head>
+<body>
+	<form method="POST">
+		<select name="idade">
+			<option></option>
+			<option value="1" <?php echo ($idade == 1)?'selected = "selected"':''; ?>>Por idade Maior</option>
+			<option value="2" <?php echo ($idade == 2)?'selected = "selected"':''; ?>>Por idade Menor</option>
+		</select>
+		<input type="submit" value="Filtrar" />
+	</form>
+	<br/>
+	<table border="1" width="100%">
+		<tr>
+			<th>Nome</th>
+			<th>Sexo</th>
+			<th>Idade</th>
+		</tr>
+	
+	<?php
+		$sexos = array(
+			'0' => 'Feminino',
+			'1' => 'Masculino'
+		);
+		
+		if($sql->rowCount() > 0){
+			foreach($sql->fetchAll() as $usuario):
+			?>
+			<tr>
+				<td><?php echo $usuario['nome']; ?></td>
+				<td><?php echo $sexos[$usuario['sexo']]; ?></td>
+				<td><?php echo $usuario['idade']; ?></td>
+			</tr>
+			<?php
+			endforeach; 	
+		}
+		
+
+	 ?>
+	 </table>
+</body>
+</html>
 	<!-- <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap4-js/bootstrap.bundle.min.js"></script> -->

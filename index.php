5fdc9b47e6ec34d5895272d1ff21bc562b469b64
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 16:22:36 -0300

    HTML5- Flexbox-Alinhamentos

--- index.php
+++ index.php
@@ -7,26 +7,32 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css">
 	<title>Projeto Bootstrap 4</title>
 	<style type="text/css">		
-		.containere{
+		.containere {
 			display:flex;
-			flex-direction:column;
-			background-color:#DDD;
-			margin-bottom:5px;
-		}
-		.container2{
-			display:flex;
+			height:500px;
 			flex-direction:row;
+			justify-content: center;
+			align-items: flex-start;
 			background-color:#DDD;
+		
 		}
-
-		.containere div,
-		.container2 div {
-			flex:1; /* quando coloca isso, fala que é do grupo 1, entao todas as div irao se dividir dentro do grupo 1, caso tenha uma div com flex:2 entao o grupo 2 vai ter o dobro do flex: 1*/
-			background-color:red;
+		.containere div {
+			width:100px;
+			height:100px;
+			background-color:#FF0000;
 			margin:10px;
 			padding:10px;
 			font-size:18px;
-			font-family: Arial;
+			
+		}
+		.div10 {
+			order:-1; <?php //assim o div vai ficar atras de todas as outras que sao order 0 no padrao?>
+			align-self: flex-start;
+			
+			<?php //O align-self serve para alinhar especificamente essa div, e ignorar o alinhamento que esta na div container, meio que sobrescreve para alinhamento nele mesmo?>
+		}
+		.div5 {
+			order:1; <?php // todas as divs estao automaticamente na ordem 0, caso coloque order 1 a div 15 ira ficar depois de todas as outras divs, porque primeiro vem as divs order 0 e depois a 1, depois a 2 e por assim vai... ?>
 		}
 
 	</style>
@@ -37,11 +43,18 @@
 		<div>1</div>
 		<div>2</div>
 		<div>3</div>
-	</div>
-	<div class="container2">
-		<div>1</div>
-		<div>2</div>
-		<div>3</div>
+		<div>4</div>
+		<div class="div5">5</div>
+		<div>6</div>
+		<div>7</div>
+		<div>8</div>
+		<div>9</div>
+		<div class="div10">10</div>
+		<div>11</div>
+		<div>12</div>
+		<div>13</div>
+		<div>14</div>
+		<div>15</div>
 	</div>
 			
 

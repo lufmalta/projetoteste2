da60419f63c7aadd048ab32ad52ab424f35effcf
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 13:03:52 -0300

    Bootstrap4- Modal-imagem-ampliada

--- index.php
+++ index.php
@@ -17,40 +17,31 @@
 <body>
 	
 	<div class="container" >
-		<button class="btn btn-link" data-toggle="modal" data-target="#janela">Fazer Login</button>  
+		<img src="assets/images/foto1.jpg" data-toggle="modal" data-target="#janela">  
 		<div class="modal fade modal-dismissible" id="janela" role="modal">
 			<div class="modal-dialog  modal-lg">
-				<div class="modal-content">
-					<div class="modal-header d-flex align-items-center">
-						<h3 style="flex:12;text-align: center;color:#CCC">Efetuar Login</h3>
-						<button style="flex:1;" class="close" data-dismiss="modal">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
+				<div class="modal-content">				
 					<div class="modal-body">
-						<form method="POST">
-							<div class="form-group">								
-								<label style="color:#CCC;" for="email" >Email:</label>
-								<input class="form-control" id="email" type="email" name="email"/>
+						<div class="row" >
+							<div class="col">
+								<button class="close" data-dismiss="modal" style="border:1px solid #ccc;background-color:red;">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>							
+						</div>
+						<div class="row">
+							<div class="col">
+								<img src="assets/images/foto1.jpg" class="w-100">
 							</div>
-							<div class="form-group">								
-								<label style="color:#CCC;" for="senha" >Senha:</label>
-								<input class="form-control" id="senha" type="password" name="senha"/>
-							</div>
-							<div class="form-group">
-								<input class="btn btn-primary form-control" type="submit" value="Login"/>
-							</div>
-						</form>
+						</div>
+							
 										
-					</div>
-					<div class="modal-footer">
-						<button class="btn btn-danger" data-dismiss="modal">Fechar Janela</button>
+
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-
 			
 
 			

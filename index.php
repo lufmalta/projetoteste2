60e37de5c6b080ca60336c184376985bab780f43
Luiz Fernando <lufmalta@gmail.com>
2018-09-19 21:57:15 -0300

    PHP Manipulando imagens

--- index.php
+++ index.php
@@ -1,81 +1,25 @@
 <?php 
-session_start();
-require 'config.php';
-if(empty($_SESSION['caixa_banco'])){
-	header("Location: login.php");
-	exit;
-}else {
-	
-	$id = $_SESSION['caixa_banco'];
+
+	$imagem = "fotocanal.png";
 
 
-	$sql = $pdo->prepare("SELECT * FROM contas WHERE id = :id");
-	$sql->bindValue(":id", $id);
-	$sql->execute();
-	if($sql->rowCount() > 0){
-		$info = $sql->fetch();
-		
-	}else { 
-		header("Location: login.php");
-		exit;
-	}
-}
+	list($largura_original, $altura_original) = getimagesize($imagem);
+	list($largura_mini, $altura_mini) = getimagesize("cruz.png");
 
+	$imagem_final = imagecreatetruecolor($largura_original, $altura_original);
+
+	$imagem_original = imagecreatefrompng("fotocanal.png");
+	$imagem_mini = imagecreatefrompng("cruz.png");
+
+	imagecopy($imagem_final, $imagem_original, 0, 0, 0, 0,
+	 $largura_original, $altura_original);
+
+	imagecopy($imagem_final, $imagem_mini, 1200, 400, 0, 0,
+		$largura_mini, $altura_mini);
+
+	//header("Content-Type: image/png");
+	//imagepng($imagem_final, "imagem_nova_tamanho.png"); Cria a imagem com o nome, 
+	//neste caso a imagem tem duas imagem na mesma, sendo uma a marca d'agua.
+
+	echo "Imagem criada com sucesso!";
 ?>
-
-
-<html>
-<head>
-	<title>Caixa Eletronico</title>
-</head>
-<body>
-	<h3>Conta Banco</h3>
-	<form method="POST">
-		Titular: <?php echo $info['titular']; ?></br>
-		Agencia: <?php echo $info['agencia']; ?></br>
-		Conta: <?php echo $info['conta']; ?></br>
-		Saldo: <?php echo $info['saldo']; ?>
-	</form>
-	<a style="text-decoration:none;color:#FF0000;" href="sair.php">Sair</a>
-	<hr/>
-	<h3>Movimentacao/ Extrato</h3>
-	<a href="add-transacao.php">Adicionar Transacao</a></br></br>
-
-	<table border="1" width="400">
-		<tr>
-			<th>Data</th>
-			<th>Valor</th>
-		</tr>
-		<?php 
-			$sql = $pdo->prepare("SELECT * FROM historico WHERE id_conta = :id_conta");
-			$sql->bindValue(":id_conta", $id);
-			$sql->execute();
-
-			if($sql->rowCount() > 0 ){
-				foreach($sql->fetchAll() as $item): ?>
-				<tr>
-					<td><?php echo date("d/m/Y H:i", strtotime($item["data_operacao"])); ?></td>
-					<td>
-						<?php 
-							if($item['tipo'] == '0'): ?>
-							<font color="green" >R$ <?php echo $item['valor']; ?> </font>
-							<?php
-							else: ?>
-							<font color="red" >- R$ <?php echo $item['valor']; ?> </font>
-							
-					  <?php endif; ?> 						
-							
-
-						
-						
-					</td>
-				</tr>
-				<?php endforeach;
-			}	
-
-		?>
-	</table>
-</br>
-	<a href="limpar.php">Limpar historico</a>
-</body>
-</html>

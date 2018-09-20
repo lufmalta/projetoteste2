9116e7573c25308b3b30ebb8be3dd8daadaf5d3b
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 13:02:52 -0300

    Bootstrap4- Modal-formulario-login

--- index.php
+++ index.php
@@ -17,29 +17,31 @@
 <body>
 	
 	<div class="container" >
-		<button class="btn btn-primary" data-toggle="modal" data-target="#janela">Clique aqui para abrir a janela</button>
+		<button class="btn btn-link" data-toggle="modal" data-target="#janela">Fazer Login</button>  
 		<div class="modal fade modal-dismissible" id="janela" role="modal">
-			<div class="modal-dialog modal-dialog-centered modal-lg">
+			<div class="modal-dialog  modal-lg">
 				<div class="modal-content">
 					<div class="modal-header d-flex align-items-center">
-						<h3 style="flex:12;text-align: center;">Carro novo</h3>
+						<h3 style="flex:12;text-align: center;color:#CCC">Efetuar Login</h3>
 						<button style="flex:1;" class="close" data-dismiss="modal">
 							<span aria-hidden="true">&times;</span>
 						</button>
 					</div>
 					<div class="modal-body">
-						<div class="row">
-							<div class="col">
-								<img src="assets/images/foto1.jpg" class="w-100"  >
-							</div>							
-						</div>
-						<hr/>
-						<div class="row">
-							<div class="col">
-								<img src="assets/images/foto2.jpg"class="w-100"  >
+						<form method="POST">
+							<div class="form-group">								
+								<label style="color:#CCC;" for="email" >Email:</label>
+								<input class="form-control" id="email" type="email" name="email"/>
 							</div>
-						</div>
-						
+							<div class="form-group">								
+								<label style="color:#CCC;" for="senha" >Senha:</label>
+								<input class="form-control" id="senha" type="password" name="senha"/>
+							</div>
+							<div class="form-group">
+								<input class="btn btn-primary form-control" type="submit" value="Login"/>
+							</div>
+						</form>
+										
 					</div>
 					<div class="modal-footer">
 						<button class="btn btn-danger" data-dismiss="modal">Fechar Janela</button>

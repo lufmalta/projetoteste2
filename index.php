a1fb89824eb52d7965d1b8b5eb9e3eb487e69b0e
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 16:51:26 -0300

    PHP-OO- Utilizando construtor

--- index.php
+++ index.php
@@ -1,64 +1,32 @@
 <!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css"> -->
 
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>PHP Orientado a objetos</title>
-	</head>
-	<body>
-		
-	</body>
-</html>
 <?php 
-	class Post {
+	class Post{
 		private $titulo;
 		private $data;
 		private $corpo;
 		private $comentarios;
 
+	public function __construct(){
+		$this->titulo = "Titulo nao definido";
+		$this->data = "Data nao definida";
+		$this->corpo = "Corpo nao definido";
+		$this->comentarios =  "Corpo nao definido";
+	}	
+
+	public function setTitulo($t){
+		if(is_string()){
+			$this->titulo = $t;
+		}
+		 
+	}	
 	public function getTitulo(){
 		return $this->titulo;
 	}
-
-	public function getData(){
-		return $this->data;
-	}
-
-	public function getCorpo(){
-		return $this->corpo;
-	}
-
-	public function getComentarios(){
-		return $this->comentarios;
-	}
-
-	public function setTitulo($t){
-		$this->titulo = $t;
-	}
-
-	public function setData($dt){
-		$this->data = $dt;
-	}
-
-	public function setCorpo($corp){
-		$this->corpo = $corp;
-	}
-
-	public function setComentarios($coment){
-		$this->comentarios = $coment;
-	}
-
-	}
+}
 
 $post = new Post();
-$post->setTitulo("novo titulo");
-$post->setData("11/07/1990");
-$post->setCorpo("Este é espaço");
-$post->setComentarios("Nao tem nada aqui");
-
-echo "Titulo: ".$post->getTitulo()."</br> Data: ".$post->getData()."</br> Corpo: ".$post->getCorpo().
-"</br> Comentarios: ".$post->getComentarios();	
-
+echo $post->getTitulo();
 
 ?>
 

d38bec8b7754216f146a84173cd33b3849b98744
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 11:17:41 -0300

    Bootstrap4 - Aula justify-content e align-items

--- index.php
+++ index.php
@@ -3,30 +3,30 @@
 <head>
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-	<!-- Traduzindo este meta: O width=device-width faz com que a width do site seja igual a width do dispositivo. O initial-scale=1 fala que o zoom inicial da pagina sera de 1 e n䯠devera ser alterado pelo navegador, porque o bootstrap ja vai fazer essa fun褯. E o shrink-to-fit=no, serve para o navegador n䯠encolher os conteudos da pagina, no caso da pagina ficar melhor, e deixar que o bootstrap fa衠isso. --> 
+	<!-- Traduzindo este meta: O width=device-width faz com que a width do site seja igual a width do dispositivo. O initial-scale=1 fala que o zoom inicial da pagina sera de 1 e não devera ser alterado pelo navegador, porque o bootstrap ja vai fazer essa função. E o shrink-to-fit=no, serve para o navegador não encolher os conteudos da pagina, no caso da pagina ficar melhor, e deixar que o bootstrap faça isso. --> 
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css">
 	<title>Projeto Bootstrap 4</title>
 	<style type="text/css">
-		.row [class^=col-], .row .col { /*esse comando class^=col-, faz pegar todas as classes que tiverem pelo menos a palavra col-, ou seja 顣omo o like do banco de dados, like col-%;*/
+		.row [class^=col-], .row .col { /*esse comando class^=col-, faz pegar todas as classes que tiverem pelo menos a palavra col-, ou seja é como o like do banco de dados, like col-%;*/
 			background-color:#DDD;
 			border:1px solid #CCC;
 		}
+
+
 	</style>
 </head>
 <body>
-	<div class="container" ">
-		<div class="row">
-			<div class="col-sm-1">Home</div>
-			<div class="col-sm-1">Contato</div>
-			<div class="col-sm-10">Visite-nos</div>
-			<!-- <div class="w-100"></div> assim vocꡦaz uma nova linha e coloca as outras colunas que sobraram nela, a outra maneira 顣riando uma nova linha -->
-		</div>	
-		<div class="row">
-			<div class="col-sm">...</div>
-			<div class="col-sm">...</div>
-			<div class="col-sm">...</div>
+	
+	<!-- <section style="background-color:red;height:50px;"> -->
+		<div class="container" style="background-color: blue;">
+			<div class="row justify-content-center" style="padding:10px;">
+				<div class="col-3" ">Primeira</div>
+				<div class="col-3 ">Segunda</div>
+				<div class="col-3">Terceira</div>
+			</div>
 		</div>
-	</div>
+	<!-- </section> -->
+	
 	
 
 

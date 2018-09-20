562d3167be7841f28cb9a3a6ab76fc697feeed76
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 13:06:02 -0300

    Bootstrap4- Paginacao

--- index.php
+++ index.php
@@ -17,28 +17,13 @@
 <body>
 	
 	<div class="container">
-
-		<nav class="navbar navbar-expand-sm navbar-light bg-light"> <!-- esse expand serve para fazer o botao aparecer s󠱵ando estiver no tamanho small -->
-
-			<a class="navbar-brand" href="#">Empresa XYZ</a>
-
-			<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu"> <!-- esse botao s󠡰arece quando o tamanho estiver em formato celular -->
-				<span class="navbar-toggler-icon"></span> <!-- esse 顯 icone de menu que vai aparecer no botao -->
-			</button>
-			<div class="navbar-collapse collapse" id="navbarMenu">
-				<div class="navbar-nav"> <!-- esse conteudo aqui dentro aparece normalmente, todavia quando ficar do tamanho small ele some, e so aparece novamente se apertar o botao do menu -->
-					<a href="" class="nav-item nav-link">Link 1</a>
-					<a href="" class="nav-item nav-link">Link 2</a>
-					<a href="" class="nav-item nav-link">Link 3</a>
-					<a href="" class="nav-item nav-link">Link 4</a>
-				</div>
-			</div>
-			<form method="POST" class="form-inline"> <!-- esse inline serve para a pesquisa ficar na mesma linha -->
-
-				<input type="text" class="form-control" placeholder="Pesquisar"/>
-			</form>
-			
-		</nav>	
+		<ul class="pagination justify-content-center">
+			<li class="page-item "><a class="page-link" href="#">1</a></li>
+			<li class="page-item "><a class="page-link" href="#">2</a></li>
+			<li class="page-item"><a class="page-link" href="#">3</a></li>
+			<li class="page-item"><a class="page-link" href="#">4</a></li>
+			<li class="page-item"><a class="page-link" href="#">5</a></li>
+		</ul>	
 	</div>
 			
 

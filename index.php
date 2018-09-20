3f953104af36bdeec2eb5d4a16a2cc65dbbcaaf7
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 11:22:14 -0300

    Bootstrap4- Media component

--- index.php
+++ index.php
@@ -3,29 +3,67 @@
 <head>
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-	<!-- Traduzindo este meta: O width=device-width faz com que a width do site seja igual a width do dispositivo. O initial-scale=1 fala que o zoom inicial da pagina sera de 1 e não devera ser alterado pelo navegador, porque o bootstrap ja vai fazer essa função. E o shrink-to-fit=no, serve para o navegador não encolher os conteudos da pagina, no caso da pagina ficar melhor, e deixar que o bootstrap faça isso. --> 
+	 
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css">
 	<title>Projeto Bootstrap 4</title>
 	<style type="text/css">
-		.row [class^=col-], .row .col { /*esse comando class^=col-, faz pegar todas as classes que tiverem pelo menos a palavra col-, ou seja é como o like do banco de dados, like col-%;*/
-			background-color:#DDD;
-			border:1px solid #CCC;
-		}
+	.avatar {
+		width:50px;
+		height:50px;
+	}		
 
 
 	</style>
 </head>
 <body>
 	
-	<!-- <section style="background-color:red;height:50px;"> -->
-		<div class="container" style="background-color: blue;">
-			<div class="row justify-content-center" style="padding:10px;">
-				<div class="col-3" ">Primeira</div>
-				<div class="col-3 ">Segunda</div>
-				<div class="col-3">Terceira</div>
+			<div class="container" >
+
+				<div class="media">
+					<img src="assets/images/cruz.png" class="mr-3 avatar align-self-center"> <!-- esse mr-3 e a margin para separar a imagem do texto -->
+					<div class="media_body">
+						<h5>Mudande site</h5>
+						<p>Aqui esta meu , se iser fazer mais um novo entlique no botao enviar e escreva antes a sua mensagem, para que seja formulado e anotado todas as infos</p>
+
+					</div>
+				</div>
+				<hr/>
+				<h3>Outra maneira de fazer isso, usando uma lista:</h3>
+
+				<ul class="list-unstylied" >
+					<li class="media">
+						<img src="assets/images/cruz.png" class="mr-3 avatar align-self-center"> <!-- esse mr-3 e a margin para separar a imagem do texto -->
+						<div class="media_body">
+						<h5>Mudande site</h5>
+						<p>Aqui esta meu fazer mais um no, clique no botao enviar e escreva antes a sua mensagem, para que seja formulado e anotado todas as informas</p>
+
+						</div>						
+					</li>
+					<li class="media">
+						<img src="assets/images/cruz.png" class="mr-3 avatar align-self-center"> <!-- esse mr-3 e a margin para separar a imagem do texto -->
+						<div class="media_body">
+						<h5>Mudande site</h5>
+						<p>Aqui esta meu clique no botao enviar e escreva antes a sua mensagem, para que seja formulado e anotado todas as informas</p>
+
+						</div>
+					</li>
+					<li class="media" >
+						<img src="assets/images/cruz.png" class="mr-3 avatar align-self-center""> <!-- esse mr-3 e a margin para separar a imagem do texto -->
+						<div class="media_body" >
+						<h5>Mudan site</h5>
+						<p>Aqui esta no botao enviar e escreva antes a sua mensagem, para que seja formulado e anotado todas as informas</p>
+
+						</div>
+					</li>
+				</ul>
+
 			</div>
-		</div>
-	<!-- </section> -->
+
+			
+
+			
+		
+	
 	
 	
 

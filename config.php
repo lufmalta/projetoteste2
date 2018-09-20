a4de407470bd4c9f369cdb5f84e030188723c497
Luiz Fernando <lufmalta@gmail.com>
2018-09-19 22:08:27 -0300

    PHP - Projeto esqueci senha(atualizado)

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
-try{
-	$pdo = new PDO("mysql:dbname=projeto_esqueciasenha;host=localhost","root", "");
-	$pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-	echo "Erro: ".$e->getMessage();
-}
+	try{
+		$pdo = new PDO("mysql:dbname=projeto_esqueciasenha;host=localhost", "root", "");
+		$pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+	}catch(PDOException $e){
+		echo "Erro: ".$e->getMessage();
+	}
 
 ?>
--- redefinir.php
+++ redefinir.php
@@ -1,50 +1,54 @@
 <?php 
 require "config.php";
-if(!empty($_GET['token'])){
-	$token = $_GET['token'];
+if(!empty($_GET['token'])): //inicia o if
+	$hash = addslashes($_GET['token']); //recebe o valor do token
 
-	$sql = "SELECT * FROM usuarios_token WHERE hash = :hash AND  used = 0
-	 AND expirado_em > NOW()";
+	?>
+	<form method="POST">
+		Nova Senha:<br/>
+		<input type="text" name="senha" /><br/><br/>
+		<input type="submit" value="Alterar"/>
+		<button><a style="text-decoration:none;color:black;" href="index.php">Voltar</a></button>
+	</form>
+
+	<?php
+
+	$sql = "SELECT * FROM usuarios_token WHERE hash = :hash AND used = 0 AND expirado_em > NOW()";
 	$sql = $pdo->prepare($sql);
-	$sql->bindValue(":hash", $token);
+	$sql->bindValue(":hash", $hash);
 	$sql->execute();
 
 	if($sql->rowCount() > 0){
 		$sql = $sql->fetch();
 		$id = $sql['id_usuario'];
 
-		if(!empty($_POST['senha'])){
-			$senha = addslashes($_POST['senha']);
-			$sql = "UPDATE usuarios SET senha = :senha WHERE id = :id";
-			$sql = $pdo->prepare($sql);
-			$sql->bindValue(":senha",md5($senha));
-			$sql->bindValue(":id", $id);
-			$sql->execute();
+		if(isset($_POST['senha']) && !empty($_POST['senha'])){
+		$senha = addslashes($_POST['senha']);
 
-			$sql = "UPDATE usuarios_token SET used = 1 WHERE hash = :hash";
-			$sql = $pdo->prepare($sql);
-			$sql->bindValue(":hash", $token);
-			$sql->execute();
+		$sql = "UPDATE usuarios SET senha = :senha WHERE id = :id";
+		$sql = $pdo->prepare($sql);
+		$sql->bindValue(":senha", md5($senha));
+		$sql->bindValue(":id", $id);
+		$sql->execute();
 
-			echo "Senha alterada com sucesso";
-			exit;
-		}
-		?>
-		<form method="POST">
-			Digite a nova senha:<br/>
-			<input type="password" name="senha"/><br/>
-			<input type="submit" value="Alterar Senha"/>
-		</form>
-		<?php 
-	}else {
-		echo "Token invalido ou usado!";
-		?><br/><a href="index.php">Voltar</a><?php
-		exit;
+		$sql = "UPDATE usuarios_token SET used = 1 WHERE hash = :hash";
+		$sql = $pdo->prepare($sql);
+		$sql->bindValue(":hash", $hash);
+		$sql->execute();
+
+		echo "Senha alterada com sucesso";
+
+
+
 	}
 
-}
+	}else {
+		echo "Alguma dessas opcoes ocorreu: Token Invalido/Senha ja foi alterada/Token ja expirado";
+	}
+// end if rowCount()
+
+	
 
 
-
+endif;// encerra o  primeiro if deste codigo
 ?>
-

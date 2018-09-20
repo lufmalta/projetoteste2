9114b94f140d2b328defb5ad6018a364b0d9e8de
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 13:01:54 -0300

    Bootstrap4 - Modal

--- index.php
+++ index.php
@@ -16,61 +16,37 @@
 </head>
 <body>
 	
-	<div class="container" style="margin-bottom:200px;">
-		
-		<div class="list-group">
-			<div class="list-group-item active">Item 1</div>
-			<div class="list-group-item">Item 2</div>
-			<div class="list-group-item disabled">Item 3</div>
+	<div class="container" >
+		<button class="btn btn-primary" data-toggle="modal" data-target="#janela">Clique aqui para abrir a janela</button>
+		<div class="modal fade modal-dismissible" id="janela" role="modal">
+			<div class="modal-dialog modal-dialog-centered modal-lg">
+				<div class="modal-content">
+					<div class="modal-header d-flex align-items-center">
+						<h3 style="flex:12;text-align: center;">Carro novo</h3>
+						<button style="flex:1;" class="close" data-dismiss="modal">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="row">
+							<div class="col">
+								<img src="assets/images/foto1.jpg" class="w-100"  >
+							</div>							
+						</div>
+						<hr/>
+						<div class="row">
+							<div class="col">
+								<img src="assets/images/foto2.jpg"class="w-100"  >
+							</div>
+						</div>
+						
+					</div>
+					<div class="modal-footer">
+						<button class="btn btn-danger" data-dismiss="modal">Fechar Janela</button>
+					</div>
+				</div>
+			</div>
 		</div>
-		<hr/>
-		<div class="list-group list-group-flush">
-			<a href="#" class="list-group-item list-group-item-action list-group-item-success">Link para item 1</a>
-			<a href="#" class="list-group-item list-group-item-action ">Link para item 2</a>
-			<a href="#" class="list-group-item list-group-item-action list-group-item-danger">Link para item 3</a>
-		</div>
-		<hr/>
-		<ul class="list-group"> <!-- isso aqui nao sei como ficou assim, por causa do botao criado, mas ficou legal -->
-			<li class="list-group-item btn btn-primary">Aqui tem 1</li>
-			<li class="list-group-item btn btn-primary">Aqui tem 2</li>
-			<li class="list-group-item btn btn-primary">Aqui tem 3</li>
-
-		</ul>
-		<hr/>
-		<ul class="list-group">
-			<li class="list-group-item d-flex justify-content-between align-items-center">
-				Teste 1
-				<span class="badge badge-primary badge-pill">15</span>
-			</li>
-			<li class="list-group-item">Teste 2</li>
-			<li class="list-group-item">Teste 3</li>
-
-		</ul>
-		<hr/>
-		<ul class="list-group">
-			<li class="list-group-item list-group-item-action">
-				<div class="d-flex justify-content-between align-items-center">
-					<h3>Noticia 1</h3>
-					<h5 class="badge badge-primary badge-pill">2 dias atras</h5>
-				</div>
-					<p>Aqui tem o conteudo da noticia que aconteceu por volta das 15:00 da tarde</p>
-				
-			</li>
-			<li class="list-group-item list-group-item-action">
-				<div class="d-flex justify-content-between align-items-center">
-					<h3>Noticia 2</h3>
-					<h5 class="badge badge-primary badge-pill">3 dias atras</h5>
-				</div>
-				<p>Aqui esta a noticia que aconteceu por causa do homem que morreu na praia</p>
-			</li>
-			<li class="list-group-item list-group-item-action">
-				<div class="d-flex justify-content-between align-items-center">
-					<h3>Noticia 3</h3>
-					<h5 class="badge badge-primary badge-pill">4 dias atras</h5>
-				</div>
-				<p>Aqui tem a noticia da pessoa que comeu goiaba no almoço e infartou</p>
-			</li>
-		</ul>
 	</div>
 
 			

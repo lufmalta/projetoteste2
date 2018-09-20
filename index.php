041da9c41f8264a256d7280d1d1c2d0bd00adffd
Luiz Fernando <lufmalta@gmail.com>
2018-09-20 16:58:22 -0300

    PHP-OO- Exemplo polimorfismo

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css"> -->
 
 <?php 
-interface Personagem{
+ class Personagem {
+ 	private $nome;
 
-	public function andar();// pode colocar também os parâmetros se houver, exemplo: $x, $y.
+ 	public function getNome(){
+ 		echo "Avante e ataque";// aqui tem o getNome da classe que o guerreiro ira herdar, quando usar o getNome, ele ira usar a funcao da classe que foi herdada
+ 	}
+ }
 
-}
+ class Guerreiro extends Personagem{
+ 	
+ 	public function getNome(){
+ 		echo "Recue e Defenda";// agora se for criado uma funcao com o mesmo nome da classe que foi herdada, entao quando chamar o método getNome ira chamar o metodo da classe herdeira, que no caso é a classe guerreiro. isso se chama polimorfismo, quando um metodo da classe herdeira substitui um da classe herdada.
+ 	}
 
-class Guerreiro implements Personagem {
-
-	public function andar(){
-		echo "Estou andando....";
-	}
-}
+ }
 
 $guerreiro = new Guerreiro();
-$guerreiro->andar();
+echo $guerreiro->getNome();
+
 
 ?>
 	<!-- <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>

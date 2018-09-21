43fa7b775c0e7021cd874fa6de1f345ea5e4905b
Luiz Fernando <lufmalta@gmail.com>
2018-09-21 14:18:58 -0300

    jQuery- Projeto- Força senha

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,53 @@
-
-function addBola(){
-
-	var x = Math.floor(Math.random()*800);
-	var y = Math.floor(Math.random()*500);
-	var cores = ['A','B','C','D','E','F','1','2','3','4','5','6','7','8','9'];
-	var numCor;
-	var cor = '#';
-	for($i=0;$i <= 5; $i++ ){
-		numCor = (Math.floor(Math.random()*14));
-		cor = cor + cores[numCor];
-
-	}	
-
-
-	var bola = $('<div class="bola"></div>');
-	bola.css('left',x+'px');
-	bola.css('top',y+'px');
-	bola.css('background-color',cor);
-
-	bola.bind('click', function(){
-
-		$(this).fadeOut('fast');
-		placar++;
-		updatePlacar();
-
-
-	});
-
-	$(document.body).append(bola);
-
-}
-
-var placar = 0;
-var comecar;
-var parar;
 $(function(){
-	$('#comecar').bind('click', function(){
-		comecar = setInterval(addBola, 1000);
-		$('#comecar').attr('disabled', 'disabled');
-		$('#parar').removeAttr('disabled');
 
-	});
+	$('#senha').bind('keyup', function(){
 
-	$('#parar').bind('click', function(){
+		var txt = $(this).val();
+		var forca = 0;
+		
+		//$('#forca').html(txt); 
 
-		parar = clearInterval(comecar);
-		$('#parar').attr('disabled', 'disabled');
-		$('#comecar').removeAttr('disabled');
+		//letras
+		//numeros
+		//caracteres especiais
+		//min de caracteres
+		if(txt.length > 15){
+			forca += 20;
+		}
+		if(txt.length > 6){
+			forca += 20;
+		}
+
+		var reg = new RegExp(/[a-z]/i);
+
+		if(reg.test(txt)){
+			forca += 20;
+		}
+
+		var reg = new RegExp(/[0-9]/i);
+
+		if(reg.test(txt)){
+			forca += 20;
+		}
+
+		var reg = new RegExp(/[^a-z0-9]/i);
+		if(reg.test(txt)){
+			forca += 20;
+		}
+		if(forca == 0){
+
+		}else if(forca == 20){
+			$('#forca').html('Senha: '+'<span style="color:red;">Muito fraca</span> ' + ' - <span style="color:red;"> Nao Aceita</span>');
+		}else if(forca == 40){
+			$('#forca').html('Senha: '+'<span style="color:red;">Fraca</span> ' + '- <span style="color:red;"> Nao Aceita</span>');
+		}else if(forca == 60){
+			$('#forca').html('Senha: '+'<span style="color:green;"> Media</span> ' + '- <span style="color:green;"> Aceita</span>');
+		}else if(forca == 80){
+			$('#forca').html('Senha: '+'<span style="color:green;"> Forte</span> ' + '- <span style="color:green;"> Aceita</span>');
+		}else if(forca == 100){
+			$('#forca').html('Senha: '+'<span style="color:green;"> Muito forte</span> ' + '- <span style="color:green;"> Aceita</span>');
+		}
 
 	});
 
 });
-function updatePlacar(){
-	$('#placar').html(placar);
-}
--- index.php
+++ index.php
@@ -6,41 +6,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 	<!-- <link 	rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css"/> -->
 	<style type="text/css">
-		.placar {
-			width:150px;
-			height:150px;
-			position:fixed;
-			left:0;
-			top:0;
-			background-color:#DDD;
-			border:1px solid #999;
-			text-align:center;
-		}
-		#placar {
-			padding: 0px 5px;
-			font-size:50px;
-			border:1px solid #CCC;
-		}
-		.button {
-			background-color:#FFF;
-		}
-		.bola {
-			width:50px;
-			height:50px;
-			border-radius:25px;
-			position:absolute;
-		}
 		
 	</style>
 
 </head>
 <body >
-	<div class="placar">
-		Bolas estouradas:<br/>
-		<span id="placar">0</span><br/><br/>
-		<button class="button" id="comecar">Começar o Jogo</button>
-		<button class="button" id="parar">Parar o Jogo</button>
-	</div>		
+	<input id="senha" type="password" name="senha" placeholder="Digite a senha...">
+
+	<div id="forca">Senha:</div>	
 	
 
 	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>

c52a0de0de46c5aa5d50cc0d6697bdfa9174c038
Luiz Fernando <lufmalta@gmail.com>
2018-09-22 00:28:03 -0300

    jQuery- Projeto-jogo da velha

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,180 @@
+var vez = '3';
+var marcacao = '3';
+var campo;
+
+
+
 $(function(){
 
-	$('a.galeria').bind('click', function(){
-		var caminho = $(this).find('img').attr('src');
-		$('.divbox img').attr('src', caminho);
+	
+	$('.placar img').attr('src', 'assets/images/foto'+vez+'.jpg');
+	var img = $(this).find('img').attr('src');
+	$('#reiniciar').attr('disabled','');
 
-		$('.bgbox, .divbox').fadeIn('fast');	
+	$('.campo').bind('click', function(){
+		var selecionado = $(this).attr('data-marcado');
+		if(selecionado == ''){
 
+			$(this).html('<img/>');			
+			$(this).find('img').attr('style', 'padding:15px;width:100px;border:0px;');
+			$(this).find('img').attr('src', img);
+			$(this).attr('data-marcado', vez);
+
+			if(vez == 3){
+				$(this).find('img').attr('src', 'assets/images/foto'+vez+'.jpg' );
+				vez = 4;
+				
+			}else if(vez == 4) {
+				$(this).find('img').attr('src', 'assets/images/foto'+vez+'.jpg' );
+				vez = 3;
+				
+			}
+			for($i = 0; $i <= 1; $i ++ ){
+				
+				if($('.a1').attr('data-marcado') == marcacao && $('.a2').attr('data-marcado') == marcacao &&
+				 $('.a3').attr('data-marcado') == marcacao  ){
+					if(marcacao == '3'){
+						alert("Carro Laranja ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						//campo = '';						
+						//$('.campo').html(campo);
+					}else if(marcacao == '4'){
+						alert("Carro Prata ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}
+					
+				}else if($('.b1').attr('data-marcado') == marcacao && $('.b2').attr('data-marcado') == marcacao &&
+				 $('.b3').attr('data-marcado') == marcacao  ){
+					if(marcacao == '3'){
+						alert("Carro Laranja ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}else if(marcacao == '4'){
+						alert("Carro Prata ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}
+				}else if($('.c1').attr('data-marcado') == marcacao && $('.c2').attr('data-marcado') == marcacao &&
+				 $('.c3').attr('data-marcado') == marcacao  ){
+					if(marcacao == '3'){
+						alert("Carro Laranja ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}else if(marcacao == '4'){
+						alert("Carro Prata ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}
+				}else if($('.a1').attr('data-marcado') == marcacao && $('.b2').attr('data-marcado') == marcacao &&
+				 $('.c3').attr('data-marcado') == marcacao  ){
+					if(marcacao == '3'){
+						alert("Carro Laranja ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}else if(marcacao == '4'){
+						alert("Carro Prata ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}
+				}else if($('.a3').attr('data-marcado') == marcacao && $('.b2').attr('data-marcado') == marcacao &&
+				 $('.c1').attr('data-marcado') == marcacao  ){
+					if(marcacao == '3'){
+						alert("Carro Laranja ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}else if(marcacao == '4'){
+						alert("Carro Prata ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}
+				}else if($('.a1').attr('data-marcado') == marcacao && $('.b1').attr('data-marcado') == marcacao &&
+				 $('.c1').attr('data-marcado') == marcacao  ){
+					if(marcacao == '3'){
+						alert("Carro Laranja ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}else if(marcacao == '4'){
+						alert("Carro Prata ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}
+				}else if($('.a2').attr('data-marcado') == marcacao && $('.b2').attr('data-marcado') == marcacao &&
+				 $('.c2').attr('data-marcado') == marcacao  ){
+					if(marcacao == '3'){
+						alert("Carro Laranja ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}else if(marcacao == '4'){
+						alert("Carro Prata ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}
+				}else if($('.a3').attr('data-marcado') == marcacao && $('.b3').attr('data-marcado') == marcacao &&
+				 $('.c3').attr('data-marcado') == marcacao  ){
+					if(marcacao == '3'){
+						alert("Carro Laranja ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}else if(marcacao == '4'){
+						alert("Carro Prata ganhou!!!");
+						window.location.href="index.php";
+						$i = 1;
+						// campo = '';						
+						// $('.campo').html(campo);
+					}
+				}else if($('.a1').attr('data-marcado') != '' && $('.a2').attr('data-marcado') != '' &&
+				 $('.a3').attr('data-marcado') != '' && $('.b1').attr('data-marcado') != ''
+				 && $('.b2').attr('data-marcado') != '' && $('.b3').attr('data-marcado') != ''
+				 && $('.c1').attr('data-marcado') != ''  && $('.c2').attr('data-marcado') != ''
+				 && $('.c3').attr('data-marcado') != ''){
+					alert('Deu velha!!!');
+					window.location.href="index.php";
+					$i = 1;
+				}
+
+				if(marcacao == '3'){
+					marcacao = '4';
+				}else {
+					marcacao = '3';
+				}
+
+			}
+			
+		}
+		$('.placar img').attr('src', 'assets/images/foto'+vez+'.jpg');
+	
 	});
 
-	$('.bgbox, .divbox button').bind('click', function(){
-
-			$('.bgbox, .divbox').fadeOut('fast');
-
-
-	});
 });
 
 
@@ -42,31 +203,3 @@ $(function(){
 
 
 
-// $(function(){
-
-// 	$('#objeto').bind('mousedown', function(){
-
-// 		$('#objeto').bind('mousemove', function(e){
-
-// 		var x = e.originalEvent.pageX;	
-// 		var y = e.originalEvent.pageY;
-
-
-// 		var width = $(this).width();
-// 		var height = $(this).height();
-
-					
-// 			//console.log(e.originalEvent.pageY);// para olhar isso é só mandar aparecer no
-// 			// console.log o 'e' e depois entrar nele e descobrir a pagex e pagey
-// 			$(this).css('left', (x - (width/2)) +'px' );
-// 			$(this).css('top', (y - (height/2)) +'px');
-// 		});
-
-// 	});
-
-// 	$('#objeto').bind('mouseup', function(){
-
-// 		$('#objeto').unbind('mousemove');
-// 	});
-
-// });
--- index.php
+++ index.php
@@ -7,60 +7,68 @@
 	<link 	rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css"/>
 	<style type="text/css">
 
-	.bgbox {
-		width:100%;
-		height:100%;
-		background-color:rgba(0, 0, 0, 0.7);
-		position:fixed;
-		left:0px;
-		top:0px;
-		display:none;
+	.campo {
+		width:100px;
+		height:100px;
+		border:3px solid #000;
+	
 	}
-	.divbox {
-		width:500px;
-		height:400px;
-		background-color:#FFF;
-		position:fixed;
-		display:none;
-		left:50%;
-		top:50%;
-		margin-left:-255px;
-		margin-top:-205px;
-		padding:5px;
+	.campo-part1 {
+		margin-left:10px;
+		float:left;
 	}
+	.campo-part2 {
+		
+		float:left;
+	}
+	.campo-part3 {
+	
+		float:left;
+	}
+	a {
+		position:absolute;
+		left:10px;
+		top:400px;
 
-
+	}
 	</style>
+
 
 </head>
 <body >
-	
-	<a href="javascript:;" class="galeria"><img src="assets/images/foto2.jpg" border="0" width="150"></a>
-	<a href="javascript:;" class="galeria"><img src="assets/images/foto3.jpg" border="0" width="150"></a>
+	<div class="placar">
+		<span style="margin-left:10px;"><strong>É a vez de:</strong></span><br/>
+		<img src="" width="50" style="margin-left:10px;"><br/><br/>
+	</div>
+	<img src="">
 
-	<div class="bgbox"></div>
-	<div class="divbox">
-		<img src="" border="0" width="100%">
-		<button style="margin:15px;">Fechar</button>
+	<div class="campo-part1">
+		<div class="campo a1" data-marcado=""></div>
+		<div class="campo b1" data-marcado=""></div>
+		<div class="campo c1" data-marcado=""></div>
+	</div>
+	<div class="campo-part2">
+		<div class="campo a2" data-marcado=""></div>
+		<div class="campo b2" data-marcado=""></div>
+		<div class="campo c2" data-marcado=""></div>
+	</div>
+	<div class="campo-part3">
+		<div class="campo a3" data-marcado=""></div>
+		<div class="campo b3" data-marcado=""></div>
+		<div class="campo c3" data-marcado=""></div>
+	</div>
+	<br/>
+	<div>
+		<a href="index.php">Reiniciar</a>
 	</div>
 
-
-
-
-	
-
-
-
-
-
-
-
-	
 
 	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
 
 	<script type="text/javascript" src="assets/js/bootstrap4-js/bootstrap.bundle.min.js"></script>
 	<script type="text/javascript" src="assets/js/script.js"></script>
+
+
 
 	
 </body>

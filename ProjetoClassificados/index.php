a22f6c3a68137a6d5cbe5b13aeb95f4542aa0000
Luiz Fernando <lufmalta@gmail.com>
2018-10-03 20:10:13 -0300

    Criando site Classificados-separando header do site também

--- ProjetoClassificados/index.php
+++ ProjetoClassificados/index.php
@@ -1,1 +1,14 @@
-Ola Pessoal!!!
+<?php require 'pages/header.php'; ?>
+	<div class="container-fluid" style="margin-top:10px;">
+		<div class="jumbotron">
+			<h2>Aqui tem 999 anuncios</h2>
+			<p>Visite o site hoje</p>
+		</div>
+		<div class="row">
+			<div class="col-sm-3">...</div>
+			<div class="col-sm-9">...</div>
+		</div>
+	</div>
+	
+</body>
+</html>
---
+++ ProjetoClassificados/pages/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Classificados</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.bundle.min"></script>
+	<script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+<body>
+	<nav style="background-color:#333;"class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="./" class="navbar-brand">Classificados</a> <!-- brand significa marca -->
+			</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="./">Cadastre-se</a></li>
+				<li><a href="./">Login</a></li>
+			</ul>
+		</div>
+	</nav>
